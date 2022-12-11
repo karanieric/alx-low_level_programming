7ace8bb8caf62c8b149f8df24a6b18fd408808ca
karanieric <erickarani53@gmail.com>
2022-12-10 22:42:32 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *main - Entry point
  *Return: Always 0 (Success)
  */
-int main(void)
+int main(void)   /*print out below sentence*/
 
 {
 puts("\"Programming is like building a multilingual puzzle"); 
