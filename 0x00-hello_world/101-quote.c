35f936e7f594d94a3c76cff09a774708c4158134
karanieric <erickarani53@gmail.com>
2022-12-13 12:38:05 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main
+ * main- entry point
  * always return 1
  */
 int main(void)
 {
-	write(I,"and that piece of art is useful\" -Dora Korpar,  2015-10-19\n", 59);
-	return(1);
+	fputs("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
