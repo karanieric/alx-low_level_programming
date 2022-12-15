1cd1754b3b0abaa3d251f8a13c151060614fa0ed
karanieric <erickarani53@gmail.com>
2022-12-14 16:34:51 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,7 @@
 /**
  * print_sign - check numers
  * @n : number to check
- *
  * Return:0 or 1
- *
  **/
 
 int print_sign(int n)
@@ -14,7 +12,6 @@ int print_sign(int n)
 	int test;
 
 	if (n > 0)
-
 	{
 			test = 1;
 			_putchar('+');
@@ -22,7 +19,6 @@ int print_sign(int n)
 	}
 
 	else if (n == 0)
-
 	{
 			test = 0;
 			_putchar('0');
@@ -30,7 +26,6 @@ int print_sign(int n)
 	}
 
 	else
-
 	{
 			test = -1;
 			_putchar('-');
