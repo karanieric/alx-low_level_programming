86c056fede14fdeac353bff466e0ab90970b9965
karanieric <erickarani53@gmail.com>
2023-01-12 10:47:34 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-main.c
+++ 0x08-recursion/5-main.c
@@ -3,25 +3,27 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ *   Return: Always 0.
  */
+
 int main(void)
 {
-
     int r;
 
         r = _sqrt_recursion(1);
-	    printf("%d\n", r);
+    printf("%d\n", r);
         r = _sqrt_recursion(1024);
+    printf("%d\n", r);
+       r = _sqrt_recursion(16);
 	printf("%d\n", r);
-        r = _sqrt_recursion(16);
-	    printf("%d\n", r);
        r = _sqrt_recursion(17);
-	    printf("%d\n", r);
-        r = _sqrt_recursion(25);
 	printf("%d\n", r);
-        r = _sqrt_recursion(-1);
+	r = _sqrt_recursion(25);
 	    printf("%d\n", r);
+        r = _sqrt_recursion(-1);
+
+	printf("%d\n", r);
+
         return (0);
 
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,7 +24,9 @@ int _sqrt(int n, int i)
 	if ((i * i) > n)
 	return (-1);
 	if (i * i == n)
-	return (i);
-	return (_sqrt(n, i + 1));
+
+							return (i);
+
+					return (_sqrt(n, i + 1));
 
 }
