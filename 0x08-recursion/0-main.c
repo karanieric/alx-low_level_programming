6b7af65f7e0d693c2c66a21f63d4ae39639b48f5
karanieric <erickarani53@gmail.com>
2023-01-12 10:34:47 -0800

    0-main.c

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * main - check the code
- * Return: Always 0.
- */
+ *
+ *  Return: Always 0.
+ **/
 int main(void)
 {
-	_puts_recursion("Puts with recursion");
-        return (0);
+	 _puts_recursion("Puts with recursion");
+
+	        return (0);
 
 }
